009bd0671e46ab360ec493c7cd702fb30e91046c
José Victor <josevictorpatos@gmail.com>
2021-11-27 16:47:49 -0300

    Creating Xcode project

---
+++ RestaurantMenu/RestaurantMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantMenu
+//
+//  Created by Jose Victor Pereira Costa on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
