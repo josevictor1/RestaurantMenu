005c0c3f780f9af101e3cc219e3ea3539a8b173f
José Victor <josevictorpatos@gmail.com>
2021-11-27 22:12:12 -0300

    Setting up MainCoordinator with window and set MenuViewController as root screen

--- RestaurantMenu/RestaurantMenu/Coordinator/MainCoordinator.swift
+++ RestaurantMenu/RestaurantMenu/Coordinator/MainCoordinator.swift
@@ -5,4 +5,25 @@
 //  Created by Jose Victor Pereira Costa on 27/11/21.
 //
 
-import Foundation
+import Core
+import UIKit
+
+final class MainCoordinator: NavigationCoordinator {
+ 
+    var parent: Coordinator?
+    var children = [Coordinator]()
+    var navigationController: UINavigationController?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        navigationController?.setViewControllers([makeMenuViewController()], animated: true)
+    }
+    
+    private func makeMenuViewController() -> MenuViewController {
+        let menuViewController = MenuViewController()
+        return menuViewController
+    }
+}
