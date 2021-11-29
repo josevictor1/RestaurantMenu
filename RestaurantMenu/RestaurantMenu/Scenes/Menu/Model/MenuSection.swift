27c49d55d6895dca21b0439febf834ad04827ba9
José Victor <josevictorpatos@gmail.com>
2021-11-28 23:38:18 -0300

    Creating MenuSection

--- RestaurantMenu/RestaurantMenu/Scenes/Menu/Model/MenuSection.swift
+++ RestaurantMenu/RestaurantMenu/Scenes/Menu/Model/MenuSection.swift
@@ -1,8 +1,15 @@
 //
-//  MenuSections.swift
+//  MenuSection.swift
 //  RestaurantMenu
 //
 //  Created by Jose Victor Pereira Costa on 28/11/21.
 //
 
 import Foundation
+
+struct MenuSection {
+    let title: String
+    var isEnabled: Bool
+}
+
+extension MenuSection: Hashable { }
