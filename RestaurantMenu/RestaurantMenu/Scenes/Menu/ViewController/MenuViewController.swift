bd20f125114ea39cb098415c68d20508e4c53344
José Victor <josevictorpatos@gmail.com>
2021-11-28 23:39:06 -0300

    Integrating MenuView with MenuViewController and setting up actions

--- RestaurantMenu/RestaurantMenu/Scenes/Menu/ViewController/MenuViewController.swift
+++ RestaurantMenu/RestaurantMenu/Scenes/Menu/ViewController/MenuViewController.swift
@@ -5,19 +5,66 @@
 //  Created by Jose Victor Pereira Costa on 27/11/21.
 //
 
+import Core
 import UIKit
 
 final class MenuViewController: UIViewController {
     
     private let menuView = MenuView()
+    var viewModel: MenuViewModelProtocol?
     
     override func loadView() {
         view = menuView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+    }
+    
+    private func setUp() {
+        setUpNavigationBarAppearence()
+        setUpMenuViewActions()
+    }
+    
+    private func setUpMenuViewActions() {
+        menuView.onSectionTapped = { [weak self] section in
+            self?.viewModel?.selectMenuSection(atIndex: section)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadRestaurantData()
+    }
+    
+    private func loadRestaurantData() {
+        startLoading()
+        viewModel?.loadRestaurantData()
+    }
+    
+    private func setUpNavigationBarAppearence() {
+        navigationController?.navigationBar.standardAppearance = .montserratNavigationBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = .montserratNavigationBarAppearance
     }
 }
 
+extension MenuViewController: MenuViewModelDelegate {
+  
+    func didFetchRestaurantName(_ restaurantName: String) {
+        title = restaurantName
+        stopLoading()
+    }
+
+    func didChangeMenuItems(_ menuItems: [MenuItem]) {
+        menuView.set(menuItems: menuItems)
+    }
+    
+    func didChangeMenuSections(_ menuSections: [MenuSection]) {
+        menuView.set(menuSections: menuSections)
+    }
+    
+    func didFailedOnLoadRestaurantData() {
+        stopLoading()
+    }
+}
